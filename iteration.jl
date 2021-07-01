71ac844dc33b65183ef4aa92e976275c8b7047a4
jamils <jamils@vt.edu>
2021-07-01 16:13:33 -0400

    Made some updates

--- iteration.jl
+++ iteration.jl
@@ -1,82 +1,36 @@
 for k ∈ 1:RK_order
-    if n == 1 && k == 1
-        dt_min = 1e10;
-        # Set primitive variables
+    println("n = $n, k = $k")
+    global U, U_RK, Res, Volume, dt, dt_min, V, T, VL, VR, VB, VT, F, G, MaxSpeed, L2, Error, Iter;
 
-        println("Initialize")
-
-        V = initialize(xc_g, yc_g);
-
-        T = computeTemperature(V);
-
-        if case == 1 # MMS Curvilinear mesh
-            S = solveSourceMMS(xc, yc);
-            V_MMS = solveSolutionMMS(xc_g, yc_g);
-            V = setBCMMS(V_MMS);
-
-            # !!!!!!!! Output to DB
-
-        else
-            S = zeros(jmax, imax);
-            V = setBC(ni_x, ni_y, nj_x, nj_y, T);
-        end
-
-        U = prim2cons(V);
-        
-        VL, VR, VB, VT = MUSCL(V);
-
-        F, G = compute2DFlux(ni_x, ni_y, nj_x, nj_y, VL, VR, VB, VT);
-
-        Res = computeRes(F, G, S, Ai, Aj, Volume); # Residuals
-
-        MaxSpeed = computeMaxSpeed(V);
-
-        dt, dt_min = computeTimeStep(
-            Volume, Ai, Aj, ni_x, ni_y, nj_x,nj_y, MaxSpeed, dt_min, V);
-        
-        U_RK = RungeKutta(U, Res, Volume, dt, dt_min);
-        V = cons2prim(U_RK);
-        U = U_RK;
-
-        L2 = computeL2(Res);
-
-        if case == 1 # Compute error for MMS case
-            Error = computeError(V, V_MMS);
-            Inter = computeL2(Error);
-        end
-            println("Starting Main Loop")
-    end
-
-    println("RK = $k")
-
-    println("RK")
     U_RK = RungeKutta(U, Res, Volume, dt, dt_min);
     V = cons2prim(U_RK);
-    println("Temp")
     T = computeTemperature(V);
     if case != 1
-        println("BC")
         V = setBC(ni_x, ni_y, nj_x, nj_y, T);
     end
 
-    println("MUSCL")
     VL, VR, VB, VT = MUSCL(V);
-    println("2DFlux")
     F, G = compute2DFlux(ni_x, ni_y, nj_x, nj_y, VL, VR, VB, VT);
-    println("Residuals")
     Res = computeRes(F, G, S, Ai, Aj, Volume);
+    U = prim2cons(V);
 end
 
 MaxSpeed = computeMaxSpeed(V);
-println("dt")
 dt, dt_min = computeTimeStep(
     Volume, Ai, Aj, ni_x, ni_y, nj_x, nj_y, MaxSpeed, dt_min, V);
 
 U = U_RK;
-println("L2")
 L2 = computeL2(Res);
 
 if case == 1
     Error = computeError(V, V_MMS);
     Iter = computeL2(Error);
 end
+
+n += 1;
+
+print(" ")
+heatmap(U[:,:,2])
+# lineplot(V[:,begin,4])
+
+# !!!!!!!! OUTPUT TO DB MORE
--- main.jl
+++ main.jl
@@ -29,17 +29,57 @@ x, y, xc, yc, xc_g, yc_g, xn, yn, Ai, Aj, ni_x, ni_y, nj_x, nj_y, Volume = value
 
 # RungeKutta Iteration
 
+dt_min = 1e10;
+# Set primitive variables
 
+println("Initialize")
+
+V = initialize(xc_g, yc_g);
+
+T = computeTemperature(V);
+
+if case == 1 # MMS Curvilinear mesh
+    S = solveSourceMMS(xc, yc);
+    V_MMS = solveSolutionMMS(xc_g, yc_g);
+    V = setBCMMS(V_MMS);
+
+    # !!!!!!!! Output to DB
+
+else
+    S = zeros(jmax, imax);
+    V = setBC(ni_x, ni_y, nj_x, nj_y, T);
+end
+
+U = prim2cons(V);
+
+VL, VR, VB, VT = MUSCL(V);
+
+F, G = compute2DFlux(ni_x, ni_y, nj_x, nj_y, VL, VR, VB, VT);
+
+Res = computeRes(F, G, S, Ai, Aj, Volume); # Residuals
+
+MaxSpeed = computeMaxSpeed(V);
+
+dt, dt_min = computeTimeStep(
+    Volume, Ai, Aj, ni_x, ni_y, nj_x,nj_y, MaxSpeed, dt_min, V);
+
+# U_RK = RungeKutta(U, Res, Volume, dt, dt_min);
+# V = cons2prim(U_RK);
+# U = U_RK;
+
+L2 = computeL2(Res);
+
+if case == 1 # Compute error for MMS case
+    Error = computeError(V, V_MMS);
+    Inter = computeL2(Error);
+end
+println("Starting Main Loop")
+
+#=
 for n ∈ 1:nmax
-    println("n = $n")
+    global U, U_RK, Res, Volume, dt, dt_min, V, T, VL, VR, VB, VT, F, G, MaxSpeed, L2, Error, Iter;
     for k ∈ 1:RK_order
-        if n == 1 && k == 1
-            dt_min = 1e10;
-            V, T, S, U, U_RK, VL, VR, VB, VT, F, G, Res, MaxSpeed, dt, dt_min, L2, Error, Inter = firstTimeStep(dt_min);
-            println("Starting Main Loop")
-
-            return U, Res, Volume, dt, dt_min
-        end
+        println("n = $n, k = $k")
 
         U_RK = RungeKutta(U, Res, Volume, dt, dt_min);
         V = cons2prim(U_RK);
@@ -51,6 +91,7 @@ for n ∈ 1:nmax
         VL, VR, VB, VT = MUSCL(V);
         F, G = compute2DFlux(ni_x, ni_y, nj_x, nj_y, VL, VR, VB, VT);
         Res = computeRes(F, G, S, Ai, Aj, Volume);
+
     end
 
     MaxSpeed = computeMaxSpeed(V);
@@ -65,9 +106,7 @@ for n ∈ 1:nmax
         Iter = computeL2(Error);
     end
 
-    return U
-
     # !!!!!!!! OUTPUT TO DB MORE
 end
-
+=#
 println("Done!!!")
